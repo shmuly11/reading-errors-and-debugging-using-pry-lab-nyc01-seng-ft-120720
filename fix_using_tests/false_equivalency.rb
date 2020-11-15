ce1d0cdba047af7705929f1d1d52fbb6879f4b77
shmuly levitin <shmuly11@gmail.com>
2020-11-15 20:51:51 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -1,5 +1,5 @@
 # don't forget to add: require 'pry'
-require 'pry'
+
 def get_user_input
   gets.chomp
 end
@@ -15,7 +15,7 @@ def selection(num)
   if num == 1
     "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2
-   binding.pry
+
     "HAM HAM HAM IN MY TUMMY"
   end
 end
