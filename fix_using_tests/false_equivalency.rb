d515ff7ec56bf31690e31b45605cd3d087d32ceb
shmuly levitin <shmuly11@gmail.com>
2020-11-15 20:51:23 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -12,9 +12,9 @@ end
 
 def selection(num)
   binding.pry
-  if num = 1
+  if num == 1
     "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
+  elsif num == 2
    binding.pry
     "HAM HAM HAM IN MY TUMMY"
   end
